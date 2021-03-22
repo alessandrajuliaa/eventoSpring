ae1c9a0b35cd059a6938500f3707cb2da8021771
Alessandra <julialessandr@gmail.com>
2021-03-22 14:50:09 -0300

    Início do projeto spring

---
+++ src/main/java/com/alessandrajulia/eventosSpring/EventosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.alessandrajulia.eventosSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alessandrajulia/eventosSpring/EventosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessandrajulia.eventosSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
